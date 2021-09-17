f7e4f0767f557d88b8f0267b66937a46da991ac2
BartdeBoer2001 <bartdb2001@gmail.com>
2021-09-17 09:44:49 +0200

    les 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Enemy
+    {
+        static void Tower(int health, int attackDamage)
+        {
+            health = 10;
+            attackDamage = 1;
+        }
+    }
+}
