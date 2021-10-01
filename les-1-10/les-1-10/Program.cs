12644d4c684c3fcfeab97cd0d3bf6fd260803c09
Bart de Boer <bartdb2001@gmail.com>
2021-10-01 10:48:04 +0200

    opdrachten laatste 2 weken

---
+++ les-1-10/les-1-10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace les_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Room
+    {
+        int[][] coordinaten = new int[10][];
+
+        public Room(int width, int height)
+        {
+        
+        }
+
+        public void Coordinates(int worldx, int worldy)
+        {
+            
+        }
+    }
+
+    class Dungeon
+    {
+        Dictionary<string, Room> rooms = new Dictionary<string, Room>();
+        rooms.Add//("Room1", new Room());
+    }
+
+    class Enemy
+    {
+        List<Enemy> enemies = new List<Enemy>();
+
+        public void logic()
+        {
+            
+        }
+
+        
+    }
+
+}
