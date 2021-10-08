5e91c0a650d1794e22b874edcdbacc14e1e59a13
Bart de Boer <bartdb2001@gmail.com>
2021-10-08 10:57:06 +0200

    Opdracht 1 8-10

---
+++ SpriteInheritance/SpriteInheritance/Wheelie.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Eindopdracht
+{
+    class Wheelie : Sprite
+    {
+
+        public Wheelie(RectangleF screenPosition)
+        {
+            this.screenPosition = screenPosition;
+            imageFrame = new Rectangle(95, 259, 16, 16);
+        }
+    }
+}
+
+
+
