cd34683ea2c5682e8329f0ce470d9e9d0841e695
Bart de Boer <bartdb2001@gmail.com>
2021-10-15 09:50:18 +0200

    Virtual / Override

---
+++ VirtualOverride/VirtualOverride/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VirtualOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Armor : Equipment
+        {
+            public int defense;
+            public override void Equip()
+            {
+                defense = 15;
+            }
+        }
+
+        class Weapon : Equipment
+        {
+            public int attack;
+            public override void Equip()
+            {
+                attack = 15;
+            }
+        }
+        
+        abstract class Equipment
+        {
+            public abstract void Equip();
+        }
+    }
+}
